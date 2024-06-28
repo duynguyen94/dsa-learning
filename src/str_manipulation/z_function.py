776134673b70cd5c1bc887a8401529b95b0558cc
Duy Nguyen <duy.nguyen@inkr.com>
2024-06-28 07:50:09 +0700

    add comment to explain the function

--- src/str_manipulation/z_function.py
+++ src/str_manipulation/z_function.py
@@ -3,7 +3,9 @@ def build_z_table(s):
     z_table = [0] * n
     match_left = match_right = 0
 
+    # start at 1 because the Z-value for the first character (index 0) is always 0.
     for i in range(1, n):
+        # Case 1: i is Outside the Current Matching Segment:
         if i >= match_right:
             match_left = match_right = i
 
@@ -11,16 +13,27 @@ def build_z_table(s):
                 match_right += 1
 
             z_table[i] = match_right - match_left
-            continue
 
-        if z_table[i - match_left] < match_right - i:
+        # Case 2: i is Inside the Current Matching Segment:
+        elif z_table[i - match_left] < match_right - i:
+            # handles the case where the current position i is within the range of the previously found matching segment
+            # [match_left, match_right)
+            #
+            # if z_table[i - match_left] > match_right - i means that the Z-value extends beyond
+            # the current matching segment.
+            #
+            # If the above condition is true, update z_table[i] with z_table[i - match_left]
+            # z_table[i] = z_table[i - match_left]
+            # - This means that the Z-value at position i is at least as large as the Z-value at
+            # position i - match_left, because the matching prefix extends beyond the current matching segment.
             z_table[i] = z_table[i - match_left]
             continue
 
-        match_left = i
-        while match_right < n and s[match_right] == s[match_right - match_left]:
-            match_right += 1
-        z_table[i] = match_right - match_left
+        else:
+            match_left = i
+            while match_right < n and s[match_right] == s[match_right - match_left]:
+                match_right += 1
+            z_table[i] = match_right - match_left
 
     return z_table
 
